7c7957884f8003234ffa017628b8bdaea91c0d77
SakChthv <somsakchthv@gmail.com>
2025-07-03 16:56:42 +0700

    create dark mode

--- script.js
+++ script.js
@@ -15,16 +15,49 @@ window.addEventListener("scroll", () => {
       "bg-white/50",
       // "bg-opacity-50",
       "backdrop-blur-lg",
-      "shadow-sm"
+      "shadow-sm",
+      "dark:bg-[#11001F]",
+      "dark:shadow-white/20"
     );
-    navLinks.classList.remove("bg-white/50", "shadow-sm");
+    navLinks.classList.remove(
+      "bg-white/50",
+      "shadow-sm",
+      "dark:border",
+      "darK:border-white/50",
+      "dark:bg-transparent"
+    );
   } else {
     navBar.classList.remove(
       "bg-white/50",
       // "bg-opacity-50",
       "backdrop-blur-lg",
-      "shadow-sm"
+      "shadow-sm",
+      "dark:bg-[#11001F]",
+      "dark:shadow-white/20"
     );
-    navLinks.classList.add("bg-white/50", "shadow-sm");
+    navLinks.classList.add(
+      "bg-white/50",
+      "shadow-sm",
+      "dark:border",
+      "darK:border-white/50",
+      "dark:bg-transparent"
+    );
   }
 });
+
+// Light and Dark mode
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+function toggleTheme() {
+  const isDark = document.documentElement.classList.toggle("dark");
+  localStorage.theme = isDark ? "dark" : "light";
+}
